8a99ff081ed958c4a8feeb4f469456e1438cfb2f
pedro.pacheco.a@gmail.com <pedro.pacheco.a@gmail.com>
2018-08-07 22:38:08 -0300

    Com função

--- app-expo-novo/App.js
+++ app-expo-novo/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View,Text,StyleSheet } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>OLa Funcionou!</Text>
-        
+export default  class appexponovo extends Component {
+
+  somar(n1,n2){
+    return n1 + n2;
+  }
+
+  render(){
+
+    let nome = "Pedro Pacheco"
+    
+    return(
+
+      <View>
+      <Text style={styles.texto}>Meu nome é {nome} e a soma de 2 + 2 é {this.somar(2, 2)}</Text>
       </View>
-    );
+
+      );
   }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  texto:{
+    fontSize:30
+  }
 });
